eb18d5973712bd97104e064f00bec77023669793
ibr-jabrego <31317527+ibr-jabrego@users.noreply.github.com>
2018-01-22 16:06:13 -0600

    Http Put Test Modifications and Clean up (#28)
    3.6.2-B    Modified Digest type to supported md5.
    
    Spacing cleanup on all other tests.

--- src/main/java/com/ibr/fedora/testsuite/HttpPut.java
+++ src/main/java/com/ibr/fedora/testsuite/HttpPut.java
@@ -52,7 +52,7 @@ public class HttpPut {
     }
 
     /**
-     * 3.6-A
+     * 3.6-B
      * @param host
      */
     @Test(priority = 26)
@@ -62,26 +62,24 @@ public class HttpPut {
 
         ps.append("\n26." + tl.httpPut()[0] + "-" + tl.httpPut()[1]).append("\n");
         ps.append("Request:\n");
-        final String resource =
-                RestAssured.given()
-    .auth().basic(this.username, this.password)
-                        .header("Content-Disposition", "attachment; filename=\"postCreate.txt\"")
-                        .body("TestString.")
-                        .when()
-                        .post(host).asString();
+        final String resource = RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .header("Content-Disposition", "attachment; filename=\"postCreate.txt\"")
+            .body("TestString.")
+            .when()
+            .post(host).asString();
 
-        RestAssured.given()
-    .auth().basic(this.username, this.password)
-                .header("Content-Disposition", "attachment; filename=\"putUpdate.txt\"")
-                .header("Link", "<http://www.w3.org/ns/ldp#RDFSource>; rel=\"type\"")
-                .body("TestString2.")
-                .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
-                .log().all()
-                .when()
-                .put(resource)
-                .then()
-                .log().all()
-                .statusCode(409);
+        RestAssured.given().auth().basic(this.username, this.password)
+            .header("Content-Disposition", "attachment; filename=\"putUpdate.txt\"")
+            .header("Link", "<http://www.w3.org/ns/ldp#RDFSource>; rel=\"type\"")
+            .body("TestString2.")
+            .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
+            .log().all()
+            .when()
+            .put(resource)
+            .then()
+            .log().all()
+            .statusCode(409);
 
         ps.append("\n -Case End- \n").close();
     }
@@ -94,33 +92,33 @@ public class HttpPut {
     @Test(priority = 27)
     @Parameters({"param1"})
     public void updateTriples(final String host) throws FileNotFoundException {
-    final PrintStream ps = TestSuiteGlobals.logFile();
-    ps.append("\n27." + tl.updateTriples()[0] + "-" + tl.updateTriples()[1]).append("\n");
-    ps.append("Request:\n");
+        final PrintStream ps = TestSuiteGlobals.logFile();
+        ps.append("\n27." + tl.updateTriples()[0] + "-" + tl.updateTriples()[1]).append("\n");
+        ps.append("Request:\n");
 
-    final String resource =
-    RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .contentType("text/turtle")
-    .when()
-    .post(host).asString();
-    String body = RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .when()
-    .get(resource).asString();
-    body += " <> dc:title \"some-title\" .";
+        final String resource = RestAssured.given()
+        .auth().basic(this.username, this.password)
+        .contentType("text/turtle")
+        .when()
+        .post(host).asString();
 
-    RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .contentType("text/turtle")
-    .body(body)
-    .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
-    .log().all()
-    .when()
-    .put(resource)
-    .then()
-    .log().all()
-    .statusCode(204);
+        String body = RestAssured.given()
+        .auth().basic(this.username, this.password)
+        .when()
+        .get(resource).asString();
+        body += " <> dc:title \"some-title\" .";
+
+        RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .contentType("text/turtle")
+            .body(body)
+            .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
+            .log().all()
+            .when()
+            .put(resource)
+            .then()
+            .log().all()
+            .statusCode(204);
 
     ps.append("\n -Case End- \n").close();
     }
@@ -133,35 +131,35 @@ public class HttpPut {
     @Test(priority = 28)
     @Parameters({"param1"})
     public void updateDisallowedTriples(final String host) throws FileNotFoundException {
-    final PrintStream ps = TestSuiteGlobals.logFile();
-    ps.append("\n28." + tl.updateDisallowedTriples()[0] + "-" + tl.updateDisallowedTriples()[1]).append("\n");
-    ps.append("Request:\n");
+        final PrintStream ps = TestSuiteGlobals.logFile();
+        ps.append("\n28." + tl.updateDisallowedTriples()[0] + "-" + tl.updateDisallowedTriples()[1]).append("\n");
+        ps.append("Request:\n");
 
-    final String resource =
-    RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .contentType("text/turtle")
-    .when()
-    .post(host).asString();
-    String body = RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .when()
-    .get(resource).asString();
-    body += " <> fedora:createdBy \"user\" .";
+        final String resource = RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .contentType("text/turtle")
+            .when()
+            .post(host).asString();
 
-    RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .contentType("text/turtle")
-    .body(body)
-    .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
-    .log().all()
-    .when()
-    .put(resource)
-    .then()
-    .log().all()
-    .statusCode(409);
+        String body = RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .when()
+            .get(resource).asString();
+        body += " <> fedora:createdBy \"user\" .";
 
-    ps.append("\n -Case End- \n").close();
+        RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .contentType("text/turtle")
+            .body(body)
+            .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
+            .log().all()
+            .when()
+            .put(resource)
+            .then()
+            .log().all()
+            .statusCode(409);
+
+        ps.append("\n -Case End- \n").close();
     }
 
     /**
@@ -172,36 +170,35 @@ public class HttpPut {
     @Test(priority = 29)
     @Parameters({"param1"})
     public void updateDisallowedTriplesResponse(final String host) throws FileNotFoundException {
-    final PrintStream ps = TestSuiteGlobals.logFile();
-    ps.append("\n29." + tl.updateDisallowedTriplesResponse()[0] + "-"
-    + tl.updateDisallowedTriplesResponse()[1]).append("\n");
-    ps.append("Request:\n");
+        final PrintStream ps = TestSuiteGlobals.logFile();
+        ps.append("\n29." + tl.updateDisallowedTriplesResponse()[0] + "-"
+        + tl.updateDisallowedTriplesResponse()[1]).append("\n");
+        ps.append("Request:\n");
 
-    final String resource =
-    RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .contentType("text/turtle")
-    .when()
-    .post(host).asString();
-    String body = RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .when()
-    .get(resource).asString();
-    body += " <> fedora:createdBy \"user\" .";
+        final String resource = RestAssured.given()
+             .auth().basic(this.username, this.password)
+             .contentType("text/turtle")
+             .when()
+             .post(host).asString();
+        String body = RestAssured.given()
+             .auth().basic(this.username, this.password)
+             .when()
+             .get(resource).asString();
+        body += " <> fedora:createdBy \"user\" .";
 
-    RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .contentType("text/turtle")
-    .body(body)
-    .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
-    .log().all()
-    .when()
-    .put(resource)
-    .then()
-    .log().all()
-    .statusCode(409).body(containsString("createdBy"));
+        RestAssured.given()
+             .auth().basic(this.username, this.password)
+             .contentType("text/turtle")
+             .body(body)
+             .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
+             .log().all()
+             .when()
+             .put(resource)
+             .then()
+             .log().all()
+             .statusCode(409).body(containsString("createdBy"));
 
-    ps.append("\n -Case End- \n").close();
+        ps.append("\n -Case End- \n").close();
     }
 
     /**
@@ -212,36 +209,35 @@ public class HttpPut {
     @Test(priority = 30)
     @Parameters({"param1"})
     public void updateDisallowedTriplesConstrainedByHeader(final String host) throws FileNotFoundException {
-    final PrintStream ps = TestSuiteGlobals.logFile();
-    ps.append("\n30." + tl.updateDisallowedTriplesConstrainedByHeader()[0] + "-"
-    + tl.updateDisallowedTriplesConstrainedByHeader()[1]).append("\n");
-    ps.append("Request:\n");
+        final PrintStream ps = TestSuiteGlobals.logFile();
+        ps.append("\n30." + tl.updateDisallowedTriplesConstrainedByHeader()[0] + "-"
+        + tl.updateDisallowedTriplesConstrainedByHeader()[1]).append("\n");
+        ps.append("Request:\n");
 
-    final String resource =
-    RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .contentType("text/turtle")
-    .when()
-    .post(host).asString();
-    String body = RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .when()
-    .get(resource).asString();
-    body += " <> fedora:createdBy \"user\" .";
+        final String resource = RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .contentType("text/turtle")
+            .when()
+            .post(host).asString();
+        String body = RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .when()
+            .get(resource).asString();
+        body += " <> fedora:createdBy \"user\" .";
 
-    RestAssured.given()
-    .auth().basic(this.username, this.password)
-    .contentType("text/turtle")
-    .body(body)
-    .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
-    .log().all()
-    .when()
-    .put(resource)
-    .then()
-    .log().all()
-    .statusCode(409).header("Link", containsString("constrainedBy"));
+        RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .contentType("text/turtle")
+            .body(body)
+            .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
+            .log().all()
+            .when()
+            .put(resource)
+            .then()
+            .log().all()
+            .statusCode(409).header("Link", containsString("constrainedBy"));
 
-    ps.append("\n -Case End- \n").close();
+        ps.append("\n -Case End- \n").close();
     }
 
     /**
@@ -255,25 +251,24 @@ public class HttpPut {
 
         ps.append("\n31." + tl.httpPutNR()[0] + "-" + tl.httpPutNR()[1]).append("\n");
         ps.append("Request:\n");
-        final String resource =
-                RestAssured.given()
-    .auth().basic(this.username, this.password)
-                        .header("Content-Disposition", "attachment; filename=\"postCreate.txt\"")
-                        .body("TestString.")
-                        .when()
-                        .post(host).asString();
+        final String resource = RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .header("Content-Disposition", "attachment; filename=\"postCreate.txt\"")
+            .body("TestString.")
+            .when()
+            .post(host).asString();
 
         RestAssured.given()
-    .auth().basic(this.username, this.password)
-                .header("Content-Disposition", "attachment; filename=\"putUpdate.txt\"")
-                .body("TestString2.")
-                .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
-                .log().all()
-                .when()
-                .put(resource)
-                .then()
-                .log().all()
-                .statusCode(204);
+            .auth().basic(this.username, this.password)
+            .header("Content-Disposition", "attachment; filename=\"putUpdate.txt\"")
+            .body("TestString2.")
+            .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
+            .log().all()
+            .when()
+            .put(resource)
+            .then()
+            .log().all()
+            .statusCode(204);
 
         ps.append("\n -Case End- \n").close();
     }
@@ -285,31 +280,31 @@ public class HttpPut {
     @Test(priority = 32)
     @Parameters({"param1"})
     public void putDigestResponseHeaderAuthentication(final String host) throws FileNotFoundException {
-        final String checksum = "sha1=cb1a576f22e8e3e110611b616e3e2f5ce9bdb941";
+        final String checksum = "MD5=97c4627dc7734f65f5195f1d5f556d7a";
         final PrintStream ps = TestSuiteGlobals.logFile();
         ps.append("\n32." + tl.putDigestResponseHeaderAuthentication()[0] + "-" +
         tl.putDigestResponseHeaderAuthentication()[1]).append("\n");
         ps.append("Request:\n");
-        final String resource =
-                RestAssured.given()
-    .auth().basic(this.username, this.password)
-                        .header("Content-Disposition", "attachment; filename=\"postCreate.txt\"")
-                        .body("TestString.")
-                        .when()
-                        .post(host).asString();
+
+        final String resource = RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .header("Content-Disposition", "attachment; filename=\"digestAuth.txt\"")
+            .body("TestString.")
+            .when()
+            .post(host).asString();
 
         RestAssured.given()
-    .auth().basic(this.username, this.password)
-                .header("digest", checksum)
-                .header("Content-Disposition", "attachment; filename=\"putUpdate.txt\"")
-                .body("TestString2.")
-                .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
-                .log().all()
-                .when()
-                .put(resource)
-                .then()
-                .log().all()
-                .statusCode(409);
+            .auth().basic(this.username, this.password)
+            .header("digest", checksum)
+            .header("Content-Disposition", "attachment; filename=\"digestAuth.txt\"")
+            .body("TestString.")
+            .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
+            .log().all()
+            .when()
+            .put(resource)
+            .then()
+            .log().all()
+            .statusCode(409);
 
         ps.append("\n -Case End- \n").close();
     }
@@ -323,32 +318,31 @@ public class HttpPut {
     public void putDigestResponseHeaderVerification(final String host) throws FileNotFoundException {
         final String checksum = "abc=abc";
         final PrintStream ps = TestSuiteGlobals.logFile();
-        ps.append("\n32." + tl.putDigestResponseHeaderVerification()[0] + "-" +
+        ps.append("\n33." + tl.putDigestResponseHeaderVerification()[0] + "-" +
         tl.putDigestResponseHeaderVerification()[1]).append("\n");
         ps.append("Request:\n");
-        final String resource =
-                RestAssured.given()
-    .auth().basic(this.username, this.password)
-                        .header("Content-Disposition", "attachment; filename=\"postCreate.txt\"")
-                        .body("TestString.")
-                        .when()
-                        .post(host).asString();
+
+        final String resource = RestAssured.given()
+            .auth().basic(this.username, this.password)
+            .header("Content-Disposition", "attachment; filename=\"postCreate.txt\"")
+            .body("TestString.")
+            .when()
+            .post(host).asString();
 
         RestAssured.given()
-    .auth().basic(this.username, this.password)
-                .header("digest", checksum)
-                .header("Content-Disposition", "attachment; filename=\"putUpdate.txt\"")
-                .body("TestString2.")
-                .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
-                .log().all()
-                .when()
-                .put(resource)
-                .then()
-                .log().all()
-                .statusCode(400);
+            .auth().basic(this.username, this.password)
+            .header("digest", checksum)
+            .header("Content-Disposition", "attachment; filename=\"putUpdate.txt\"")
+            .body("TestString2.")
+            .config(RestAssured.config().logConfig(new LogConfig().defaultStream(ps)))
+            .log().all()
+            .when()
+            .put(resource)
+            .then()
+            .log().all()
+            .statusCode(400);
 
         ps.append("\n -Case End- \n").close();
      }
-
 
     }
