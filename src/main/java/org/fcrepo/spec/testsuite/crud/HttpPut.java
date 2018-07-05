35a25528c89cb6082f696b93a54ef22f49bc20c9
Andrew Woods <awoods@duraspace.org>
2018-07-05 11:05:16 -0400

    Get ETag from GET not POST
    Related to: https://github.com/fcrepo4-labs/Fedora-API-Test-Suite/issues/88
    Resolves: https://github.com/fcrepo4-labs/Fedora-API-Test-Suite/issues/131

--- src/main/java/org/fcrepo/spec/testsuite/crud/HttpPut.java
+++ src/main/java/org/fcrepo/spec/testsuite/crud/HttpPut.java
@@ -97,9 +97,10 @@ public class HttpPut extends AbstractTest {
                                         "https://fcrepo.github.io/fcrepo-specification/#http-put-ldprs", ps);
         final Response resource = createBasicContainer(uri, info);
         final String locationHeader = getLocation(resource);
-        final String etag = getETag(resource);
 
-        final String body2 = doGet(locationHeader).asString();
+        final Response getResponse = doGet(locationHeader);
+        final String body2 = getResponse.asString();
+        final String etag = getETag(getResponse);
 
         final String newBody = body2.replace("Put class Container", "some-title");
 
