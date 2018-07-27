83b2201ec1d822fbb4cf87012d026ff8408d8af7
Andrew Woods <awoods@duraspace.org>
2018-07-27 14:22:20 -0400

    Collect resource headers from GET, not initial POST
    Resolves: https://github.com/fcrepo/Fedora-API-Test-Suite/issues/137

--- src/main/java/org/fcrepo/spec/testsuite/versioning/LdprvHttpGet.java
+++ src/main/java/org/fcrepo/spec/testsuite/versioning/LdprvHttpGet.java
@@ -106,10 +106,15 @@ public class LdprvHttpGet extends AbstractVersioningTest {
         //create ldprv
         final Response createResponse = createVersionedResource(uri, info);
 
-        final URI timeMapURI = getTimeMapUri(createResponse);
+        //get location of new resource
+        final String resourceUri = getLocation(createResponse);
+
+        final Response response = doGet(resourceUri);
+
+        final URI timeMapURI = getTimeMapUri(response);
 
         //get timegate uri
-        final URI timeGateUri = getTimeGateUri(createResponse);
+        final URI timeGateUri = getTimeGateUri(response);
 
         final Response newMementoResponse = doPost(timeMapURI.toString());
         final String newMementoUri = getLocation(newMementoResponse);
