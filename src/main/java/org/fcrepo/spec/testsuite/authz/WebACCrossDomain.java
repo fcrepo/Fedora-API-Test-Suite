94090fb81a888eff19fca4eeaba6a4a6b3bc5586
Daniel Bernstein <dabylon@gmail.com>
2018-10-29 15:16:42 -0700

    Implements WebACCrossDomainTests. Resolves: https://github.com/fcrepo/Fedora-API-Test-Suite/issues/262

--- src/main/java/org/fcrepo/spec/testsuite/authz/WebACCrossDomain.java
+++ src/main/java/org/fcrepo/spec/testsuite/authz/WebACCrossDomain.java
@@ -17,15 +17,23 @@
  */
 package org.fcrepo.spec.testsuite.authz;
 
-import org.fcrepo.spec.testsuite.AbstractTest;
+import static org.testng.AssertJUnit.fail;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.fcrepo.spec.testsuite.TestInfo;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 /**
  * @author awoods
  * @since 2018-07-16
  */
-public class WebACCrossDomain extends AbstractTest {
+public class WebACCrossDomain extends AbstractAuthzTest {
 
     /**
      * 5.5-A - Restrict ACLs to local resources
@@ -33,8 +41,28 @@ public class WebACCrossDomain extends AbstractTest {
     @Test(groups = {"MAY"})
     public void restrictAclToLocal() {
         final TestInfo info = setupTest("5.5-A",
-                "Implementations may restrict support for ACLs to local resources.",
-                "https://fedora.info/2018/06/25/spec/#cross-domain-acls", ps);
+                                        "Implementations may restrict support for ACLs to local resources.",
+                                        "https://fedora.info/2018/06/25/spec/#cross-domain-acls", ps);
+
+        final Response resource = createResourceWithRemoteAcl();
+        if (successRange().matches(resource.getStatusCode())) {
+            fail("This implementation does not restrict ACLs to local resources.");
+        } else {
+            resource.then().statusCode(clientErrorRange());
+        }
+
+    }
+
+    private Response createResourceWithRemoteAcl() {
+        if (!isClientAclLinkingSupported()) {
+            throw new SkipException("Client specified ACL locations not supported.");
+
+        }
+        //create a resource to be used for the acl link
+        final String aclUri = "http://example.org/my-acl";
+        //PUT the new Resource
+        final String aclLinkValue = "<" + aclUri + ">; rel=\"acl\"";
+        return doPutUnverified(uri, Headers.headers(new Header("Link", aclLinkValue)), "test body");
     }
 
     /**
@@ -43,9 +71,16 @@ public class WebACCrossDomain extends AbstractTest {
     @Test(groups = {"MUST"})
     public void rejectRemoteAclStatus() {
         final TestInfo info = setupTest("5.5-B",
-                "If an implementation chooses to reject requests concerning remote ACLs, it must respond with a " +
-                        "4xx range status code.",
-                "https://fedora.info/2018/06/25/spec/#cross-domain-acls", ps);
+                                        "If an implementation chooses to reject requests concerning remote ACLs, it " +
+                                        "must respond with a " +
+                                        "4xx range status code.",
+                                        "https://fedora.info/2018/06/25/spec/#cross-domain-acls", ps);
+
+        final Response resource = createResourceWithRemoteAcl();
+
+        if (!successRange().matches(resource.getStatusCode())) {
+            resource.then().statusCode(clientErrorRange());
+        }
     }
 
     /**
@@ -54,10 +89,20 @@ public class WebACCrossDomain extends AbstractTest {
     @Test(groups = {"MUST"})
     public void rejectRemoteAclConstraint() {
         final TestInfo info = setupTest("5.5-C",
-                "If an implementation chooses to reject requests concerning remote ACLs, it must advertise the " +
-                        "restriction with a rel=\"http://www.w3.org/ns/ldp#constrainedBy\" link in the Link " +
-                        "response header.",
-                "https://fedora.info/2018/06/25/spec/#cross-domain-acls", ps);
+                                        "If an implementation chooses to reject requests concerning remote ACLs, it " +
+                                        "must advertise the " +
+                                        "restriction with a rel=\"http://www.w3.org/ns/ldp#constrainedBy\" link in " +
+                                        "the Link " +
+                                        "response header.",
+                                        "https://fedora.info/2018/06/25/spec/#cross-domain-acls", ps);
+        final Response resource = createResourceWithRemoteAcl();
+
+        if (!successRange().matches(resource.getStatusCode())) {
+            confirmPresenceOfConstrainedByLink(resource);
+        } else {
+            resource.then().statusCode(successRange());
+        }
+
     }
 
     /**
@@ -66,8 +111,31 @@ public class WebACCrossDomain extends AbstractTest {
     @Test(groups = {"MAY"})
     public void restrictGroupToLocal() {
         final TestInfo info = setupTest("5.6-A",
-                "Implementations may restrict support for groups of agents to local Group Listing documents.",
-                "https://fedora.info/2018/06/25/spec/#cross-domain-groups", ps);
+                                        "Implementations may restrict support for groups of agents to local Group " +
+                                        "Listing documents.",
+                                        "https://fedora.info/2018/06/25/spec/#cross-domain-groups", ps);
+
+        final Response response = createResourceWithRemoteGroupListInAcl(info);
+
+        if (successRange().matches(response.getStatusCode())) {
+            throw new SkipException("This implementation does not restrict group lists to local resources.");
+        } else {
+            response.then().statusCode(clientErrorRange());
+        }
+    }
+
+    private Response createResourceWithRemoteGroupListInAcl(final TestInfo info) {
+        final String resourceUri = createResource(uri, info.getId());
+        final Map<String, String> params = new HashMap<>();
+        final String groupUri = "http://example.org/group-list";
+        params.put("groupListResource", groupUri);
+        params.put("resource", resourceUri);
+
+        //get acl handle
+        final String aclUri = getAclLocation(resourceUri);
+        //create read acl for user role
+        return doPutUnverified(aclUri, new Headers(new Header("Content-Type", "text/turtle")),
+                               filterFileAndConvertToString("group-authorization.ttl", params));
     }
 
     /**
@@ -76,9 +144,18 @@ public class WebACCrossDomain extends AbstractTest {
     @Test(groups = {"MUST"})
     public void rejectRemoteGroupStatus() {
         final TestInfo info = setupTest("5.6-B",
-                "If an implementation chooses to reject requests concerning remote Group Listings, it must respond " +
-                        "with a 4xx range status code.",
-                "https://fedora.info/2018/06/25/spec/#cross-domain-groups", ps);
+                                        "If an implementation chooses to reject requests concerning remote Group " +
+                                        "Listings, it must respond " +
+                                        "with a 4xx range status code.",
+                                        "https://fedora.info/2018/06/25/spec/#cross-domain-groups", ps);
+
+        final Response aclResponse = createResourceWithRemoteGroupListInAcl(info);
+
+        if (!successRange().matches(aclResponse.getStatusCode())) {
+            aclResponse.then().statusCode(clientErrorRange());
+        } else {
+            throw new SkipException("Implementation does not reject requests concerning remote Group Listings.");
+        }
     }
 
     /**
@@ -87,10 +164,20 @@ public class WebACCrossDomain extends AbstractTest {
     @Test(groups = {"MUST"})
     public void rejectRemoteGroupConstraint() {
         final TestInfo info = setupTest("5.6-C",
-                "If an implementation chooses to reject requests concerning remote Group Listings, it must advertise " +
-                        "the restriction with a rel=\"http://www.w3.org/ns/ldp#constrainedBy\" link in the Link " +
-                        "response header.",
-                "https://fedora.info/2018/06/25/spec/#cross-domain-groups", ps);
+                                        "If an implementation chooses to reject requests concerning remote Group " +
+                                        "Listings, it must advertise " +
+                                        "the restriction with a rel=\"http://www.w3.org/ns/ldp#constrainedBy\" link " +
+                                        "in the Link " +
+                                        "response header.",
+                                        "https://fedora.info/2018/06/25/spec/#cross-domain-groups", ps);
+
+        final Response aclResponse = createResourceWithRemoteGroupListInAcl(info);
+        if (!successRange().matches(aclResponse.getStatusCode())) {
+            aclResponse.then().statusCode(clientErrorRange());
+            confirmPresenceOfConstrainedByLink(aclResponse);
+        } else {
+            throw new SkipException("Implementation does not reject requests concerning remote Group Listings.");
+        }
     }
 
 }
